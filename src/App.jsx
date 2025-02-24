e5bb77c643d6c6dc154c1446a2e87c6896e8f131
ela.md <ela.ahmadi06@gmail.com>
2025-02-24 13:09:09 -0500

    responsive the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
-
+import { AppProvider } from './context.jsx'
 import './App.css'
+import Navbar from './components/navbar/Navbar.jsx'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
 
 
   return (
-    <>
+  <>
+    <AppProvider>
+       <Navbar />
        <h1>App</h1>
-    </>
+    </AppProvider>
+  </>
   )
 }
 
