d923cb227bfecac1c53f52eb9a3bfa1556760a9f
ela.md <ela.ahmadi06@gmail.com>
2025-02-26 14:50:34 -0500

    footer added

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import './Footer.css'
+function Footer () {
+    return(
+        <div className="footer">
+        <p>
+          developed by <strong>Ela Ahmadi</strong>
+        </p>
+      </div>
+    )
+}
+
+export default Footer
