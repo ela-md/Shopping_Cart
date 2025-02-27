ecfb9227cec3dab0378cb531f4f63f40ffa6ed66
ela.md <ela.ahmadi06@gmail.com>
2025-02-27 11:20:12 -0500

    setup add to card button

--- src/components/productitem/ProductItem.jsx
+++ src/components/productitem/ProductItem.jsx
@@ -1,19 +1,33 @@
+import { useContext } from 'react'
+import { AppContext } from '../../context'
 import './ProductItem.css'
+import database from '../../database'
+
+function ProductItem({id, image, name, price}){
+     
+const {addedToCart, setAddedToCart, addedProducts, setAddedProducts} = useContext(AppContext)
+
+const addToCartHandler = ()=> {
+setAddedToCart(addedToCart + 1)
+const product = database.find(product => product.id == id)
+product.count = 1
+product.totalPrice = product.price * product.count
+setAddedProducts(prevState => [...prevState , product]);
 
 
 
-function ProductItem({image, name, price}){
 
-    
-    return(
+}
+
+return(
         <div className="productCard">
             <div className='cardImage'>
                 <img className='img-fluid' src={image} />
             </div>
             <div className="cardBody">
                 <h5>{name}</h5>
-                <p className='price'>price: {price}</p>
-                <button className='btn btn-dark'>add to cart</button>
+                <p className='price'>price: {price.toLocaleString()}</p>
+                <button onClick={addToCartHandler}  className='btn btn-dark'>add to cart</button>
             </div>
         </div>
     )
